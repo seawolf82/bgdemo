c0f3d8ea6c7201d14f003e6a7d266c96b506fad1
seawolf82 <andrea.finessi@gmail.com>
2018-02-07 15:50:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -92,6 +92,25 @@
           <p>
             <img src="bluerose.jpeg">
           </p>
+          <?php
+           //echo phpinfo();
+           error_reporting(-1);
+           ini_set('display_errors', 'On');
+           //echo "test";
+           $conn=oci_connect("stage","stage","192.168.3.81:1521/xe");
+            if (!$conn) {
+              $e = oci_error();
+              echo $e['message'].' ciao';
+              print_r($e);
+              echo "test2";
+            }
+            $query=oci_parse($conn,"select * from anagrafica");
+            oci_execute($query);
+            while($row=oci_fetch_array($query)){
+            print_r($row);
+            }
+          oci_close($conn);
+          ?>
         </div>
       </div>
         </div><!-- /col -->
